e8885202de6edf3cc8e1f67a9a2445b9e95733a8
Manshika07 <anshikami520@gmail.com>
2025-07-31 16:38:53 +0530

    Minor Fixes

--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,7 @@ const app = express();
 // Middlewares
 app.use(cors(
     {
-        origin: ["http://localhost:5173","https://book-store-p3sj.vercel.app/"],
+        origin: ["http://localhost:5173","https://book-store-p3sj.vercel.app"],
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true
     }
